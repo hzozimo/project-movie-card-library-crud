aac513bd846a37516749a3250ea25a36d9894888
hzozimo <hzozimo@gmail.com>
2021-05-07 17:09:13 -0300

    desafio 4 feito

--- src/pages/MovieDetails.js
+++ src/pages/MovieDetails.js
@@ -1,25 +1,80 @@
 import React, { Component } from 'react';
-
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 import * as movieAPI from '../services/movieAPI';
 import { Loading } from '../components';
 
 class MovieDetails extends Component {
+  constructor() {
+    super();
+    this.state = {
+      loading: true,
+      title: '',
+      storyline: '',
+      imagePath: '',
+      genre: '',
+      rating: 0,
+      subtitle: '',
+    };
+  }
+
+  componentDidMount() {
+    const { getMovie } = movieAPI;
+    const { match } = this.props;
+    const { id } = match.params;
+    getMovie(id).then((movie) => this.setState(
+      {
+        loading: false,
+        title: movie.title,
+        storyline: movie.storyline,
+        imagePath: movie.imagePath,
+        genre: movie.genre,
+        rating: movie.rating,
+        subtitle: movie.subtitle,
+      },
+    ));
+  }
+
   render() {
     // Change the condition to check the state
     // if (true) return <Loading />;
 
-    const { title, storyline, imagePath, genre, rating, subtitle } = {};
+    const { loading, title, storyline, imagePath, genre, rating, subtitle } = this.state;
+    const { match } = this.props;
+    const { id } = match.params;
 
     return (
       <div data-testid="movie-details">
-        <img alt="Movie Cover" src={ `../${imagePath}` } />
-        <p>{ `Subtitle: ${subtitle}` }</p>
-        <p>{ `Storyline: ${storyline}` }</p>
-        <p>{ `Genre: ${genre}` }</p>
-        <p>{ `Rating: ${rating}` }</p>
+        {loading ? <Loading />
+          : (
+            <div>
+              <img alt="Movie Cover" src={ `../${imagePath}` } />
+              <p>{ `Title: ${title}` }</p>
+              <p>{ `Subtitle: ${subtitle}` }</p>
+              <p>{ `Storyline: ${storyline}` }</p>
+              <p>{ `Genre: ${genre}` }</p>
+              <p>{ `Rating: ${rating}` }</p>
+              <p>
+                <Link to={ `/movies/:${id}/edit` }>EDITAR</Link>
+                |
+                <Link to="/">VOLTAR</Link>
+
+              </p>
+            </div>
+          )}
       </div>
     );
   }
 }
+
+MovieDetails.propTypes = {
+  loading: PropTypes.bool,
+  title: PropTypes.string,
+  storyline: PropTypes.string,
+  imagePath: PropTypes.string,
+  genre: PropTypes.string,
+  rating: PropTypes.number,
+  subtitle: PropTypes.string,
+}.isRequired;
 
 export default MovieDetails;
