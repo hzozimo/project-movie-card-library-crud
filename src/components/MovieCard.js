305c8cd2eea0b85897d7ad42df1604bee537276b
hzozimo <hzozimo@gmail.com>
2021-05-07 16:38:03 -0300

    desafio 3 feito (o anterior era o 2)

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
 class MovieCard extends React.Component {
   render() {
     const { movie } = this.props;
-    const { title, storyline, imagePath } = movie;
+    const { title, storyline, imagePath, id } = movie;
     return (
       <div className="movie-card" data-testid="movie-card">
         <img alt="Movie Cover" className="movie-card-image" src={ imagePath } />
@@ -12,6 +13,7 @@ class MovieCard extends React.Component {
           <h4 data-testid="movie-card-title" className="movie-card-title">{title}</h4>
           <p className="movie-card-storyline">{storyline}</p>
         </div>
+        <Link to={ `movies/${id}` }>VER DETALHES</Link>
       </div>
     );
   }
@@ -22,6 +24,7 @@ MovieCard.propTypes = {
     title: PropTypes.string,
     storyline: PropTypes.string,
     imagePath: PropTypes.string,
+    id: PropTypes.number,
   }).isRequired,
 };
 export default MovieCard;
